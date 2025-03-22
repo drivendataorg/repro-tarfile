42d626627c01bef0b89b165f779414af0a6b3aa4
Jay Qi <jayqi@users.noreply.github.com>
2025-03-22 18:46:46 -0400

    Fix bad usage example for addfile

--- examples/usage.py
+++ examples/usage.py
@@ -1,6 +1,6 @@
 """Basic usage example of repro_tarfile."""
 
-from io import StringIO
+from io import BytesIO
 from pathlib import Path
 
 from repro_tarfile import ReproducibleTarFile, TarInfo
@@ -11,4 +11,7 @@ with ReproducibleTarFile.open(example_dir / "archive.tar.gz", "w:gz") as tar:
     # Use add to add a file to the archive
     tar.add(example_dir / "data.txt", arcname="data.txt")
     # Or addfile to write from a data stream to the archive
-    tar.addfile(TarInfo(name="lore.txt"), fileobj=StringIO("goodbye"))
+    stream = BytesIO("goodbye".encode("utf-8"))
+    tarinfo = TarInfo(name="lore.txt")
+    tarinfo.size = stream.getbuffer().nbytes
+    tar.addfile(tarinfo, fileobj=stream)
